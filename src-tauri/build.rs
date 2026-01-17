d4e557d2f31f9393ac84830e2f031c267a594f44
BlueHawky <woshipmx1991@sina.com>
2026-01-17 13:41:26 +0800

    fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,9 +2,9 @@ fn main() {
   println!("cargo:rustc-check-cfg=cfg(desktop)");
   println!("cargo:rustc-check-cfg=cfg(mobile)");
 
-  if std::env::var("TAURI_CONFIG").is_err() {
-    return;
-  }
+  // if std::env::var("TAURI_CONFIG").is_err() {
+  //   return;
+  // }
 
   tauri_build::build();
 }
